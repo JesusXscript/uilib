ee22e693f83c64bba82367cc6ba285daac7a795f
Serkrxnkub <57833538+Serkrxn@users.noreply.github.com>
2023-09-07 19:04:40 +0700

    Update uilib.lua

--- uilib.lua
+++ uilib.lua
@@ -1,3 +1,7 @@
+if game:GetService("CoreGui"):FindFirstChild("ScreenGui") then
+    game:GetService("CoreGui"):FindFirstChild("ScreenGui"):Destroy()
+    end
+
 local library = {
 	Version = "0.36",
 	WorkspaceName = "Pepsi Lib",
